465a154b5a72d67d15a3dad021d87df4ba66d979
MarcusCaue <cauemarcuscauefb@gmail.com>
2022-07-19 11:50:45 -0300

    Modularizando as ações em funções e não em arquivos

--- cadastro_login_usuarios/pages/php/cadastro_page/cadastro_functions.php
+++ cadastro_login_usuarios/pages/php/cadastro_page/cadastro_functions.php
@@ -1,7 +1,30 @@
 <?php
   function criar_conexao() {
+    // Informações necessárias para se conectar ao Banco de Dados
+    $host = "localhost";
+    $porta = 3306;
+    $user = "root";
+    $senha = "BoB?y4x999";
+    $bd = "cadastro_login_usuarios";
 
+    // Fazendo a conexão
+    return $conexao = new mysqli($host, $user, $senha, $bd, $porta);
   }
-  
+
+  function fechar_conexao($conexao_mysql) {
+    mysqli_close($conexao_mysql);
+  }
+
+  function cadastrar($conexao_mysql) {
+    # Campos 
+    $nome = $_POST["nome"];
+    $idade = $_POST["idade"];
+    $email = $_POST["email"];
+    $senha = $_POST["senha"];
+
+    # Enviando-os ao BD
+    $sql_command = "INSERT INTO usuario (nome, idade, email, senha) VALUES ('$nome', '$idade', '$email', '$senha');";
+    $resultado = mysqli_query($conexao_mysql, $sql_command);
+  }
 
 ?>
--- cadastro_login_usuarios/pages/php/cadastro_page/index.php
+++ cadastro_login_usuarios/pages/php/cadastro_page/index.php
@@ -1,11 +1,14 @@
-<!-- Importando o arquivo que realiza a conexão com o banco de dados -->
-<?php include_once("../config_bd.php"); ?>
+<!-- Importando o arquivo que contém as funções PHP que serão utilizadas -->
+<?php include_once("cadastro_functions.php"); ?>
+
+<!-- Realizando a conexão com o banco de dados -->
+<?php $conexao = criar_conexao(); ?>
 
 <!-- Enviando os dados do formulário para o banco de dados -->
-<?php include_once("cadastro.php") ?>
+<?php cadastrar($conexao); ?>
 
 <!-- Página PHP a ser exibida quando o cadastro for concluído -->
 <?php include_once("page.php") ?>
 
 <!-- Encerrando a conexão com o BD -->
-<?php mysqli_close($conexao); ?>
+<?php fechar_conexao($conexao) ?>
