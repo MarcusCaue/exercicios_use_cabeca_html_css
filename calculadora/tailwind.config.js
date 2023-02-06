c5d3e60d142842bfccaf197b98adf05ddda0b14f
Marcus Cauê <marcuscaue.dev@gmail.com>
2023-02-06 12:46:22 -0300

    Refazendo a calculadora com Tailwind

---
+++ calculadora/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        "background-1": "#0D1117",
+        "background-2": "#161B22",
+        "border-gray": "#c9d1d92e"
+      }
+    },
+  },
+  plugins: [],
+}
