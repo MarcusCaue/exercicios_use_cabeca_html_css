6c310df79793fd8e1173b251aa68a84879648f91
Marcus Cauê <marcuscaue.dev@gmail.com>
2023-02-07 20:17:04 -0300

    Calculadora "funcionando"

--- calculadora/src/scripts/script.js
+++ calculadora/src/scripts/script.js
@@ -6,17 +6,19 @@ const operatorsSymbols = {
   '<i class="fa-sharp fa-solid fa-plus"></i>': "+",
   '<i class="fa-sharp fa-solid fa-minus"></i>': "-",
   '<i class="fa-solid fa-xmark"></i>': "*",
-  '<i class="fa-solid fa-divide"></i>': "/"
+  '<i class="fa-solid fa-divide"></i>': "/",
+  '<i class="fa-solid fa-equals"></i>': "="
 }
 
 const calculadora = {
   "+": (a, b) => { return a + b },
   "-": (a, b) => { return a - b },
   "/": (a, b) => { return a / b },
-  "*": (a, b) => { return a * b }
+  "*": (a, b) => { return a * b },
+  "n1": undefined,
+  "n2": undefined,
+  "operator": undefined
 }
-
-const calculo = []
 
 // Adicionando o evento de clique para todos os botões
 allButtons.forEach(btn => {
@@ -43,6 +45,7 @@ allButtons.forEach(btn => {
 
       if (idBtn.startsWith("all")) {
         feedback.innerHTML = ""
+        calculadora.n1 = calculadora.n2 = calculadora.operator = undefined
       }
     }
 
@@ -53,44 +56,57 @@ allButtons.forEach(btn => {
       input.value = inputValue
     }
 
+    // Botão igual
+    else if (idBtn === "igual") {
+
+      if (feedback.innerHTML !== "" && input.value !== "") {
+        calculadora.n2 = Number(input.value)
+
+        if (calculadora.n2 === 0 && calculadora.operator === "/") {
+          window.alert("Não tem como dividir por 0, digite outro número")
+          return undefined
+        }
+
+        input.value = calculadora[calculadora.operator](calculadora.n1, calculadora.n2)
+        feedback.innerHTML = ""
+        calculadora.n1 = calculadora.n2 = calculadora.operator = undefined
+      } else {
+        window.alert("Insira os dados para o cálculo por favor")
+        return undefined
+      }
+
+      return undefined
+    }
+
     // Operações matemática
     else if (classesBtn.contains("operacao")) {
-      let operator = btn.innerHTML
-      let number = input.value
-      let symbolOperator = operatorsSymbols[operator]
+      let operatorIcon = btn.innerHTML
+      let inputValue = Number(input.value)
+      let symbolOperator = operatorsSymbols[operatorIcon]
 
-      if (number === "") {
+      if (inputValue === "") {
         window.alert("Digite um número")
         return undefined
       }
 
-      if (idBtn === "igual") {
-        input.value = number
-        feedback.innerHTML = ""
-        return undefined
+      if (calculadora.n1 !== undefined) {
+        calculadora.n2 = inputValue
+
+        if (calculadora.n2 === 0 && calculadora.operator === "/") {
+          window.alert("Não tem como dividir por 0, digite outro número")
+          return undefined
+        }
+
+        // Resultado da operação
+        calculadora.n1 = calculadora[calculadora.operator](calculadora.n1, calculadora.n2)
+        calculadora.operator = symbolOperator
+      } else {
+        calculadora.n1 = inputValue
+        calculadora.operator = symbolOperator
       }
-
-      if (calculo.length === 2) {
-        let n1 = calculo[0]
-        symbolOperator = calculo[1]
-        let n2 = Number(number)
-
-        let resultado = calculadora[symbolOperator](n1, n2)
-
-        calculo.pop(); calculo.pop()
-
-        input.value = resultado
-        feedback.innerHTML = ""
-        console.log(calculo)
-
-        return undefined
-      }
-
-      feedback.innerHTML += ` ${number} ${operator} `
+      
+      feedback.innerHTML = ` ${calculadora.n1} ${calculadora.operator} `
       input.value = ""
-
-      calculo.push(Number(number), symbolOperator)
-      console.log(calculo)
     }
 
     return undefined
